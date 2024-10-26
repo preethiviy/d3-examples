067c8cf41eec144cf275fe82a555cc9f0657b4aa
preethiviy <preethivirajyambem@gmail.com>
2024-10-26 23:35:10 +0530

    added linear scale along y axis

--- src/components/d3/BarChart.tsx
+++ src/components/d3/BarChart.tsx
@@ -32,15 +32,16 @@ const BarChart = () => {
 
     const margin = {
         top: 20,
-        bottom: 40,
+        bottom: 80,
         left: 40,
         right: 20,
     };
 
-    const maxAge = d3.max(barData, (d) => d.age) || 0;
     const graphWidth = svgWidth - margin.left - margin.right;
     const graphHeight = svgHeight - margin.top - margin.bottom;
     const rectWidth = Math.floor(graphWidth / barData.length);
+
+    const scaleY = d3.scaleLinear([0, 100], [0, graphHeight]);
 
     useEffect(() => {
         const svg = d3.select(myElementRef.current);
@@ -63,10 +64,10 @@ const BarChart = () => {
                 return i * rectWidth + margin.left;
             })
             .attr('y', (d) => {
-                return graphHeight - d.age + margin.top;
+                return graphHeight - scaleY(d.age) + margin.top;
             })
             .attr('height', (d) => {
-                return d.age;
+                return scaleY(d.age);
             });
 
         //draw x-axis line
@@ -86,6 +87,49 @@ const BarChart = () => {
             .attr('y2', margin.top + graphHeight)
             .attr('stroke', 'black')
             .attr('stroke-width', 2);
+
+        //create x-axis labels (name)
+        svg.selectAll('.name-label')
+            .data(barData)
+            .enter()
+            .append('text')
+            .text((d) => d.name)
+            .attr('class', 'name-label')
+            .attr('x', (d, i) => i * rectWidth + margin.left + 10)
+            .attr('y', graphHeight + margin.top)
+            .attr(
+                'transform',
+                (d, i) =>
+                    `rotate(45 ${i * rectWidth + margin.left} ${graphHeight + margin.top + 20})`,
+            )
+            .attr('fill', 'gray');
+
+        //create y-axis labels (age)
+        svg.selectAll('.age-label')
+            .data(barData)
+            .enter()
+            .append('text')
+            .text((d) => d.age)
+            .attr('class', 'age-label')
+            .attr('x', (d, i) => i * rectWidth + margin.left + rectWidth / 2)
+            .attr('y', (d) => margin.top + graphHeight - scaleY(d.age) - 5)
+            .attr('fill', '#1F77B4')
+            .attr('text-anchor', 'middle')
+            .attr('font-weight', 'bold');
+
+        //create y-axis labels - 10 value apart
+        const yAxisLabelData = d3.range(0, 100, 10);
+
+        svg.selectAll('.y-axis-label')
+            .data(yAxisLabelData)
+            .enter()
+            .append('text')
+            .text((d) => d)
+            .attr('x', margin.left - 5)
+            .attr('y', (d) => margin.top + graphHeight - scaleY(d))
+            .attr('fill', 'gray')
+            .attr('text-anchor', 'end')
+            .attr('alignment-baseline', 'middle');
     });
 
     return (
